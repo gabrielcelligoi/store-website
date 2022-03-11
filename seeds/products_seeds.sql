49e5ddc18b55e58ad6ef3958348aeb8701f262e3
Gabriel Celligoi <gabrielcelligoi@gmail.com>
2022-03-11 22:19:36 +0000

    database done

--- seeds/products_seeds.sql
+++ seeds/products_seeds.sql
@@ -1,7 +1,7 @@
 INSERT INTO products (seller_id, name, description, price, stock, image)
 VALUES
-(1, 'Cat Food', 'Provenance Series Hauraki Plains Wet recipe for Cats Named for its gentle winds', 413, 10, 'www.google.com'),
-(2, 'Dog Bike Basket', 'Replace all your other carriers with ChillBug’s all in one portable pet carrier bag.', 8995, 7, 'www.google.com'),
-(3, 'Solid Wood Cat Tree', 'Solid wood hand-made', 39900, 3, 'www.google.com'),
-(4, 'The Gripper Tactical Leash', "We know that some people just love cotton leashes and in the past, due to their lack of grip they haven't been high on our list of must haves.", 3895, 10, 'www.google.com');
+(9, 'Cat Food', 'Provenance Series Hauraki Plains Wet recipe for Cats Named for its gentle winds', 413, 10, 'www.google.com'),
+(10, 'Dog Bike Basket', 'Replace all your other carriers with ChillBug’s all in one portable pet carrier bag.', 8995, 7, 'www.google.com'),
+(11, 'Solid Wood Cat Tree', 'Solid wood hand-made', 39900, 3, 'www.google.com'),
+(12, 'The Gripper Tactical Leash', 'We know that some people just love cotton leashes and in the past, due to their lack of grip they havent been high on our list of must haves.', 3895, 10, 'www.google.com');
 
---
+++ seeds/sale_seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO sale (user_id, product_id, shipping_cost, country, street, city, province, postal)
+VALUES (1, 1, 10, 'canada', 'main st','hamilton','on', '23eew2'),
+(3, 1, 10, 'canada2', 'main st', 'hamilton', 'on', '347dgw'),
+(1, 2, 10, 'canada3', 'main st2', 'hamilton', 'on', '347dgw'),
+(7, 3, 10, 'canada4', 'main st3', 'hamilton', 'on', '347dgw'),
+(3, 4, 10, 'canada5', 'main st4', 'hamilton', 'on', '347dgw'),
+(6, 1, 10, 'canada6', 'main st5', 'hamilton', 'on', '347dgw'),
+(2, 4, 10, 'canada7', 'main st6', 'hamilton', 'on', '347dgw'),
+(5, 3, 10, 'canada8', 'main st7', 'hamilton', 'on', '347dgw');
