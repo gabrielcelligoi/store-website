d894cde3ada2d195e288819f5da1e559723d1802
Gabriel Celligoi <gabrielcelligoi@gmail.com>
2022-03-14 20:03:50 +0000

    add featured products bar

--- database.js
+++ database.js
@@ -5,22 +5,6 @@ const db = new Pool(dbParams);
 db.connect();
 
 
-// const insertNewUser = function(arrayWithUserValues, cookieName) {
-//   return db.query(`
-//   INSERT INTO users (name, email, password, country, street, city, province, postal)
-//   VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-//   RETURNING *;
-//   `, arrayWithUserValues)
-//   .then(data => {
-//     cookieName = data.rows[0].id;
-//     res.redirect("/");
-//     return data.rows[0];
-//   })
-//   .catch(error => {
-//     console.log(error.message);
-//   })
-// }
-
 const getUserWithEmail = function(email) {
   return db.query(`
   SELECT *
